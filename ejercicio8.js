34aea90a3a425544e77c3b44607f16cac2a6e600
alber <alberto.saez.canada@gmail.com>
2024-12-10 14:01:01 +0100

    hasta ej.11

---
+++ ejercicio8.js
@@ -0,0 +1,11 @@
+const avengers = ['Hulk', 'Thor', 'Iron Man', 'Captain A.', 'Spiderman', 'Captain M.'];
+/*let arrayLength=avengers.length;
+console.log(arrayLength);*/
+
+function findLongestWord(stringList) {
+  // Completar código
+
+
+}
+
+
