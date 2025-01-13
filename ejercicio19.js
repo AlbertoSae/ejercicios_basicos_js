f0de874c7730b21b199e88b7da8df7670037cdc8
alber <alberto.saez.canada@gmail.com>
2025-01-13 19:10:48 +0100

    13.01.2025

---
+++ ejercicio19.js
@@ -0,0 +1,16 @@
+const toys = [
+    {id: 5, name: 'Transformers'}, 
+    {id: 11, name: 'LEGO'}, 
+    {id: 23, name: 'Hot Wheels'}, 
+    {id: 40, name: 'Rascador de gato'},
+    {id: 40, name: 'FurReal Friends gato interactivo'},
+    {id: 60, name: 'Nerf Blaster'},
+    {id: 71, name: 'Sylvanian Families - Familia gato'}
+];
+
+for(let i=0; i<toys.length;i++){
+    if(toys[i].name.includes('gato')){
+        toys.splice(i,1);
+    }
+}
+console.log(toys);
